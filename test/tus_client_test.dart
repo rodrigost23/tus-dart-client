afe09b3547f7244390e60e7bb59a05241a5cd51b
Rodrigo Tavares <rodrigost23@users.noreply.github.com>
2020-06-15 16:27:02 -0400

    Update tests

--- test/tus_client_test.dart
+++ test/tus_client_test.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:tus_client/tus_client.dart';
@@ -6,17 +7,54 @@ import 'package:test/test.dart';
 void main() {
   late TusClient client;
   late TusUploader uploader;
+  final url = Uri.http('localhost:1080', '/files/');
 
-  setUp(() async {
-    client = TusClient(Uri.parse('http://master.tus.io/files/'));
-    uploader = await client.createUploader(file: File('./LICENSE'));
+  group('No resume support', () {
+    setUp(() async {
+      client = TusClient(url);
+      uploader = await client.createUploader(file: File('./LICENSE'));
+    });
+
+    test('Get uploader URL', () async {
+      expect(uploader.url.toString(), startsWith(client.endpoint.toString()));
+    });
+
+    test('Upload whole file', () async {
+      await uploader.upload();
+    });
   });
 
-  test('Get uploader URL', () async {
-    expect(uploader.url.toString(), startsWith(client.endpoint.toString()));
-  });
+  group('With resume support', () {
+    late TusUploader uploader2;
+    final store = TusMemoryStore();
 
-  test('Upload whole file', () async {
-    await uploader.upload();
+    setUp(() async {
+      client = TusClient(url, store: store, chunkSize: 256);
+      uploader = await client.createUploader(file: File('./LICENSE'));
+      uploader2 = await client.createUploader(file: File('./LICENSE'));
+    });
+
+    test('Store saved the URL', () async {
+      expect(store.get(uploader.fingerprint), equals(uploader.url));
+    });
+
+    test('Second uploader has the same URL as the first', () async {
+      expect(uploader2.url, equals(uploader.url));
+    });
+
+    test('Second uploader resumes download from the first', () async {
+      await uploader.uploadChunk();
+      await uploader2.upload();
+
+      print('Finished upload');
+
+      var request = await HttpClient().getUrl(uploader2.url);
+      var response = await request.close();
+
+      print('Finished download');
+
+      var downloaded = await response.transform(utf8.decoder).single;
+      expect(downloaded, uploader2.file.readAsStringSync());
+    });
   });
 }
